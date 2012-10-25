b61f299da27828a31776c4f6f0caec7471ba83ed
Georg Kunz <kwd@gmx.ch>
2012-10-25 22:39:18 +0200

    Update unicorn settings

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,5 +1,42 @@
-# http://michaelvanrooijen.com/articles/2011/06/01-more-concurrency-on-a-single-heroku-dyno-with-the-new-celadon-cedar-stack/
+APP_ROOT = Pathname.new(File.dirname(__FILE__)+'/..').realpath
+ 
+require 'bundler/setup'
 
-worker_processes 3 # amount of unicorn workers to spin up
-timeout 30         # restarts workers that hang for 30 seconds
+worker_processes 3
+working_directory APP_ROOT.to_s
 preload_app true
+timeout 5
+ 
+ 
+listen APP_ROOT.join('unicorn.sock').to_s, :backlog => 64
+pid APP_ROOT.join("tmp/pids/unicorn.pid").to_s
+ 
+Configurator::DEFAULTS[:logger].formatter = Logger::Formatter.new
+ 
+stderr_path APP_ROOT.join("log/unicorn.stderr.log").to_s
+stdout_path APP_ROOT.join("log/unicorn.stdout.log").to_s
+ 
+before_exec do |server|
+  ENV['BUNDLE_GEMFILE'] = "#{APP_ROOT}/Gemfile"
+end
+
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) && ActiveRecord::Base.connection.disconnect!
+ 
+  old_pid = APP_ROOT.join('tmp/pids/unicorn.pid.oldbin').to_s
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      Process.kill("QUIT", File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      puts "Old master alerady dead"
+    end
+  end
+end
+ 
+after_fork do |server, worker|
+  Bundler.setup
+  defined?(ActiveRecord::Base) && ActiveRecord::Base.establish_connection
+end
+
+
+
